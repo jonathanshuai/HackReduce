2c7835c9b5e9449b92d2373c7cef887e978d758a
pabloazar <pabloazar@gmail.com>
2011-06-25 18:43:27 -0400

    Screw JSON!

--- src/main/java/org/hackreduce/models/CityYearRecord.java
+++ src/main/java/org/hackreduce/models/CityYearRecord.java
@@ -2,7 +2,7 @@ package org.hackreduce.models;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
-import net.sf.json.*;
+
 
 import org.apache.hadoop.io.Writable;
 
@@ -59,16 +59,18 @@ public class CityYearRecord implements Writable {
          
      }
      
-      public JSONObject toJSON(){
-    	JSONObject result = new JSONObject();  
-    	result.element("count", this.count);
-    	result.element("city", this.cr.asciiname);
-    	result.element("long", this.cr.longitude);
-    	result.element("lat", this.cr.latitude);
-    	result.element("year", this.year);
-    	result.element("id", this.cr.geonameid);
-    	return result;
+    
       
-      }
+      public String toString(){
+      	String result = "{" ; 
+      	result += "count:" +  this.count + ",";
+      	result += "city:"  + this.cr.asciiname + ",";
+      	result += "long:" + this.cr.longitude + ",";
+      	result += "lat:" + this.cr.latitude + ",";
+      	result += "year:" + this.year + ",";
+      	result += "id:" + this.cr.geonameid + "}";
+      	return result;
+        
+        }
 	
 }
